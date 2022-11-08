a63843d391de52ba9aeb3c9f638ee1e0a5cf5208
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-11-08 10:33:26 +0600

    success page added

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Success from '../Success/Success';
 import TeamSection from '../TeamSection/TeamSection';
 
 const Home = () => {
     return (
         <div>
-  <Banner ></Banner>
-  <TeamSection></TeamSection>
+            <Banner ></Banner>
+            <TeamSection></TeamSection>
+            <Success></Success>
         </div>
     );
 };
---
+++ src/Components/Pages/Success/Success.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo1 from '../../../image/download (3).svg'
+import logo2 from '../../../image/download (4).svg'
+import logo3 from '../../../image/download (5).svg'
+import logo4 from '../../../image/download (6).svg'
+
+
+const Success = () => {
+    return (
+        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://i.ibb.co/hKTFbQg/why-choose-us1.jpg")` }}>
+
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content  text-neutral-content">
+                <div className="lg:flex  gap-32">
+                    <div>
+                        <img src={logo1} className='w-28' alt="" />
+                        <h3 className='text-5xl font-bold font-serif'>600</h3>
+                        <h3 className='text-2xl font-bold uppercase font-serif'>happiness <br /> customers</h3>
+                    </div>
+                    <div>
+                        <img src={logo2} className='w-28' alt="" />
+                        <h3 className='text-5xl font-bold font-serif'>1000</h3>
+                        <h3 className='text-2xl font-bold uppercase font-serif'>Project completed</h3>
+                    </div>
+                    <div>
+                        <img src={logo3} className='w-28' alt="" />
+                        <h3 className='text-5xl font-bold font-serif'>3000</h3>
+                        <h3 className='text-2xl font-bold uppercase font-serif'>world branches</h3>
+                    </div>
+                    <div>
+                        <img src={logo4} className='w-28' alt="" />
+                        <h3 className='text-5xl font-bold font-serif'>100</h3>
+                        <h3 className='text-2xl font-bold uppercase font-serif'>special winner </h3>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Success;
+
+//<a href="https://ibb.co/RCJTfrg"><img src="https://i.ibb.co/hKTFbQg/why-choose-us1.jpg" alt="why-choose-us1" border="0"></a>
