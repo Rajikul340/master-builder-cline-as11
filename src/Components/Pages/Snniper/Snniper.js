ae600f5087ed946eb5d8fd8d879878ae03d1b55f
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-11-10 19:53:39 +0600

    update change

--- src/Components/Pages/Snniper/Snniper.js
+++ src/Components/Pages/Snniper/Snniper.js
@@ -4,7 +4,8 @@ import React from 'react';
 export const Snniper = () => {
     return (
         <div>
-            <p>loading...</p>
+            {/* <p>loading...</p> */}
+            <progress className="progress w-full fixed-top"></progress>
         </div>
     );
 };
